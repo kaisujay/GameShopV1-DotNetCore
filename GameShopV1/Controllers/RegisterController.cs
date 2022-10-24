4166ffd9a9fa7a27a3e044a91be71e09b290e791
kaisujay <kai.sujay@gmail.com>
2022-10-24 14:17:48 +0530

    Register New Player

---
+++ GameShopV1/Controllers/RegisterController.cs
@@ -0,0 +1,34 @@
+﻿using GameShopV1.Data;
+using GameShopV1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameShopV1.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly GameShopDbContext _gameShopDbContext;
+
+        public RegisterController(GameShopDbContext gameShopDbContext)
+        {
+            _gameShopDbContext = gameShopDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Player player)
+        {
+            var newPlayer = new Player()
+            {
+                Name = player.Name,
+                Email = player.Email,
+                Age = player.Age
+            };
+            _gameShopDbContext.Players.Add(newPlayer);
+            _gameShopDbContext.SaveChanges();
+            return View();
+        }
+    }
+}
