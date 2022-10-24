41e2a69f6743b561f8e981c8889a410dfd2e27d3
kaisujay <kai.sujay@gmail.com>
2022-10-24 16:42:57 +0530

    Added Basic Login code but can not display Player Name to the UI

---
+++ GameShopV1/Controllers/LogInController.cs
@@ -0,0 +1,38 @@
+﻿using GameShopV1.Data;
+using GameShopV1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameShopV1.Controllers
+{
+    public class LogInController : Controller
+    {
+        private readonly GameShopDbContext _gameShopDbContext;
+
+        public LogInController(GameShopDbContext gameShopDbContext)
+        {
+            _gameShopDbContext = gameShopDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Player player)
+        {
+            var playerExists = _gameShopDbContext.Players
+            .Where(x => x.Email == player.Email && x.Age == player.Age)
+            .FirstOrDefault();
+
+            if (playerExists != null)
+            {
+                return RedirectToAction("Index", "Store",playerExists.Id);
+            }
+            else
+            {
+                ViewBag.AuthenticationError = "Sorry Invalid Credentials !";
+                return View();
+            }
+        }
+    }
+}
