d3b63970fce0ebe1d09a2f14fba8aaedd2e3fade
kaisujay <kai.sujay@gmail.com>
2022-10-24 12:31:16 +0530

    Added new Store Home page to display all the games

---
+++ GameShopV1/Controllers/StoreController.cs
@@ -0,0 +1,19 @@
+﻿using GameShopV1.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameShopV1.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly GameShopDbContext _gameShopDbContext;
+        public StoreController(GameShopDbContext gameShopDbContext)
+        {
+            _gameShopDbContext = gameShopDbContext;
+        }
+        public IActionResult Index()
+        {
+            var games = _gameShopDbContext.Games.ToList();
+            return View(games);
+        }
+    }
+}
